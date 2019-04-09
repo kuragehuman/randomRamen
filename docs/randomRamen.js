3fcdb9a44288f3a8ab14d95e4ab882ca919b8cce
kuragehuman <kuragehuman@gmail.com>
2019-04-09 10:25:44 +0900

    change randomRamen.js and index.html

--- docs/randomRamen.js
+++ docs/randomRamen.js
@@ -35,7 +35,7 @@ function randomSelect(){
     var oneweek = new Array("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat");
     for(var i = 0; i < csvData.length; i++){
         if(oneweek[today.getDay()] == csvData[i][1]){
-            openRestaurant.pushcsvData[i];
+            openRestaurant.push(csvData[i]);
         }
     }
     alert(openRestaurant[0]);
