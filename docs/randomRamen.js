e8666dd813eee45c5d30002c4dfc275e8bb9c675
kuragehuman <kuragehuman@gmail.com>
2019-04-18 23:42:36 +0900

    fix randomRamen.js

--- docs/randomRamen.js
+++ docs/randomRamen.js
@@ -40,7 +40,7 @@ function timeCheck(openTime, closeTime){
     var openTimeDate = new Date(today.getFullYear(), today.getMonth(), today.getDate(), Number(openTimeSplit[0]), Number(openTimeSplit[1]));
     var closeTimeDate = new Date(today.getFullYear(), today.getMonth(), today.getDate(), Number(closeTimeSplit[0]), Number(closeTimeSplit[1]));
 
-    alert(openTimeDate.getTime() + "\n" + today.getTime() + "\n" + closeTimeDate.getTime());
+    //alert(openTimeDate.getTime() + "\n" + today.getTime() + "\n" + closeTimeDate.getTime());
 
     if(openTimeDate.getTime() < today.getTime() && today.getTime() < closeTimeDate.getTime()){
         return 1;
@@ -60,7 +60,7 @@ function openCheck(restaurants){
         }
         else if(open.length%2==0){
             for(var j=0; j<open.length/2; j++){
-                alert(tmp[i].name);
+                //alert(tmp[i].name);
                 if(timeCheck(open[2*j], open[(2*j)+1]) == 1){
                     openDT.push(tmp[i]);
                     break;
